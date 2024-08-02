4306aae06b58a91a1c76fa19bc8d0350e738f389
Mateo <mateofrontend@gmail.com>
2024-08-02 14:05:52 -0300

    carrousel

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-let startX, scrollLeft, isDown = false;
 // Inicializa Swiper
 const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
-    autoplay: {
-      delay: 3000, // Tiempo de espera entre cada diapositiva (en milisegundos)
-      disableOnInteraction: false, // No deshabilitar autoplay en interacción
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.custom-button-next',
-      prevEl: '.custom-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
-  });
-  
+  speed: 400,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 3000, // Tiempo de espera entre cada diapositiva (en milisegundos)
+    disableOnInteraction: false, // No deshabilitar autoplay en interacción
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.custom-button-next',
+    prevEl: '.custom-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
