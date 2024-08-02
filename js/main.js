d48f0462d682f40dd50a4678fa12523ab42c26ce
Mateo <mateofrontend@gmail.com>
2024-08-02 10:34:42 -0300

    sections

--- js/main.js
+++ js/main.js
@@ -1,46 +1,23 @@
 let startX, scrollLeft, isDown = false;
-
-const carrusel = document.querySelector('.carrusel');
-
-carrusel.addEventListener('mousedown', (e) => {
-    isDown = true;
-    carrusel.classList.add('active');
-    startX = e.pageX - carrusel.offsetLeft;
-    scrollLeft = carrusel.scrollLeft;
-});
-
-carrusel.addEventListener('mouseleave', () => {
-    isDown = false;
-    carrusel.classList.remove('active');
-});
-
-carrusel.addEventListener('mouseup', () => {
-    isDown = false;
-    carrusel.classList.remove('active');
-});
-
-carrusel.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - carrusel.offsetLeft;
-    const walk = (x - startX) * 3; // Ajusta la velocidad del desplazamiento
-    carrusel.scrollLeft = scrollLeft - walk;
-});
-
-carrusel.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].pageX - carrusel.offsetLeft;
-    scrollLeft = carrusel.scrollLeft;
-    isDown = true;
-});
-
-carrusel.addEventListener('touchend', () => {
-    isDown = false;
-});
-
-carrusel.addEventListener('touchmove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.touches[0].pageX - carrusel.offsetLeft;
-    const walk = (x - startX) * 3; // Ajusta la velocidad del desplazamiento
-    carrusel.scrollLeft = scrollLeft - walk;
-});
+// Inicializa Swiper
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    autoplay: {
+      delay: 3000, // Tiempo de espera entre cada diapositiva (en milisegundos)
+      disableOnInteraction: false, // No deshabilitar autoplay en interacción
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.custom-button-next',
+      prevEl: '.custom-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
+  
